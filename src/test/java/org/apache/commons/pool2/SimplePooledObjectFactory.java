5d0ced15647290afc2b06b39ed21526d658876d6
Alvin Zhang <mosoft521@gmail.com>
2023-12-14 18:14:14 +0800

    Apache Commons —— Apache Commons Pool 2

---
+++ src/test/java/org/apache/commons/pool2/SimplePooledObjectFactory.java
@@ -0,0 +1,20 @@
+package org.apache.commons.pool2;
+
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class SimplePooledObjectFactory extends BasePooledObjectFactory<ComplexObject> {
+    @Override
+    public ComplexObject create() {
+        // 随机指定一个名称，用于区分ComplexObject
+        String name = "test" + ThreadLocalRandom.current().nextInt(100);
+        return new ComplexObject(name);
+    }
+
+    @Override
+    public PooledObject<ComplexObject> wrap(ComplexObject obj) {
+        // 使用默认池化对象包装ComplexObject
+        return new DefaultPooledObject(obj);
+    }
+}
